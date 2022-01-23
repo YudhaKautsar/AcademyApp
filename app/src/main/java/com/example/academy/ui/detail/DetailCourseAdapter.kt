e75a4f61748123a9b7d7ae9ea29ae935252319db
YudhaKautsar <yudhakautsar20@gmail.com>
2022-01-24 02:06:25 +0700

    Studi kasus proyek android jetpack

---
+++ app/src/main/java/com/example/academy/ui/detail/DetailCourseAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.academy.ui.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.academy.data.ModuleEntity
+import com.example.academy.databinding.ItemsModuleListBinding
+import com.example.academy.ui.detail.DetailCourseAdapter.*
+
+class DetailCourseAdapter : RecyclerView.Adapter<ModuleViewHolder>() {
+
+    private val listModules = ArrayList<ModuleEntity>()
+
+    fun setModules(modules: List<ModuleEntity>?) {
+        if (modules == null) return
+        this.listModules.clear()
+        this.listModules.addAll(modules)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ):  ModuleViewHolder {
+        val itemModuleListBinding = ItemsModuleListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ModuleViewHolder(itemModuleListBinding)
+    }
+
+    override fun onBindViewHolder(holder: ModuleViewHolder, position: Int) {
+        val module = listModules[position]
+        holder.bind(module)
+    }
+
+    override fun getItemCount(): Int = listModules.size
+
+    inner class ModuleViewHolder(private val binding: ItemsModuleListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(module: ModuleEntity) {
+            binding.textModuleTitle.text = module.title
+        }
+    }
+}
